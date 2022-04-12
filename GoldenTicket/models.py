6285510bdc355112709c785df925c5fa976773b1
Mohammed Harris <57700846+mxharryx@users.noreply.github.com>
2022-04-12 15:09:26 -0400

    stuck at merging dbs

--- GoldenTicket/models.py
+++ GoldenTicket/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Movie(models.Model):
@@ -11,5 +12,6 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.movie_title
+
 
 
--- GoldenTicket/views.py
+++ GoldenTicket/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Movie
 
 
@@ -8,6 +9,18 @@ def homepage(request):
     movies = Movie.objects.all()[:6]
     return render(request=request, template_name="GoldenTicket/main.html", context={'movies': movies})
 
+
 def movielist(request):
     movie_list = Movie.objects.all()
-    return render(request=request, template_name="GoldenTicket/movie_list.html", context={'movie_list':movie_list})
+    return render(request=request, template_name="GoldenTicket/movie_list.html", context={'movie_list': movie_list})
+
+
+def movies(request):
+    Movies = Movie.objects.all()
+    paginator = Paginator(Movies, 20)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request=request, template_name="GoldenTicket/movie_list.html", context={'page_obj': page_obj})
+
+
+Paginator = (movies, 20)
