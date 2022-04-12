179350e8d87d95b86aafa0fb5ff0497de067e07a
Mohammed Harris <57700846+mxharryx@users.noreply.github.com>
2022-04-12 05:48:21 -0400

    added data

--- GoldenTicket/models.py
+++ GoldenTicket/models.py
@@ -5,8 +5,10 @@ class Movie(models.Model):
     movie_title = models.CharField(max_length=150)
     release_year = models.IntegerField()
     director = models.CharField(max_length=100)
+    movie_poster = models.ImageField(upload_to="images", default="../static/default_avatar.png", blank=True)
     description = models.TextField()
 
     def __str__(self):
         return self.movie_title
+
 
